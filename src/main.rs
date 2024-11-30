855268c7f32674971c837837c86c3ad5194782b0
Ussama Azam <nuclearoreo@gmail.com>
2024-11-30 02:06:54 -0500

    Big refactor

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,35 @@ use env_logger;
 use flate2::read::ZlibDecoder;
 use log;
 
+fn initialize_git_repository() {
+    fs::create_dir(".git").expect("Failed to create .git directory");
+    fs::create_dir(".git/objects").expect("Failed to create .git/objects directory");
+    fs::create_dir(".git/refs").expect("Failed to create .git/refs directory");
+    fs::write(".git/HEAD", "ref: refs/heads/main\n").expect("Failed to create .git/HEAD file");
+}
+
+fn print_git_object_contents(args: &[String]) {
+    if args.len() != 2 {
+        println!("usage: my-git cat-file -t <hash>");
+        println!("usage: my-git cat-file -p <hash>");
+        std::process::exit(1);
+    }
+
+    let _obj_type = &args[0];
+    let hash = &args[1];
+
+    let content: Vec<u8> = fs::read(format!(".git/objects/{}/{}", &hash[..2], &hash[2..]))
+        .expect("Failed to read object");
+
+    let mut z_lib_decoder = ZlibDecoder::new(&content[..]);
+    let mut buffer = String::new();
+    z_lib_decoder
+        .read_to_string(&mut buffer)
+        .expect("Failed to read object content");
+
+    print!("{}", &buffer[8..]);
+}
+
 fn main() {
     // Setting logging
     env_logger::init();
@@ -15,30 +44,9 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     // Commands
-    if args[1] == "init" {
-        // Initialize git repository
-        fs::create_dir(".git").expect("Failed to create .git directory");
-        fs::create_dir(".git/objects").expect("Failed to create .git/objects directory");
-        fs::create_dir(".git/refs").expect("Failed to create .git/refs directory");
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").expect("Failed to create .git/HEAD file");
-        println!("Initialized git directory")
-    } else if args[1] == "cat-file" {
-        // Print the contents of a git object
-        // Flags: -p, -s, -t
-        let _flag = &args[2];
-        let hash = &args[3];
-
-        let content = fs::read(format!(".git/objects/{}/{}", &hash[..2], &hash[2..]))
-            .expect("Failed to read object");
-
-        let mut z_lib_decoder = ZlibDecoder::new(&content[..]);
-        let mut buffer = String::new();
-        z_lib_decoder
-            .read_to_string(&mut buffer)
-            .expect("Failed to read object content");
-
-        print!("{}", &buffer[8..]);
-    } else {
-        println!("unknown command: {}", args[1])
+    match args[1].as_str() {
+        "init" => initialize_git_repository(), // Initialize git repository
+        "cat-file" => print_git_object_contents(&args[2..]), // Print git object contents
+        _ => println!("unknown command: {}", args[1]),
     }
 }
